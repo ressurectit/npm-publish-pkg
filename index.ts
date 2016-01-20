975f17a9bf5595a11cbb3553bd07b86d726fad0b
kukko <kukjevov@gmail.com>
2016-01-20 08:18:30 +0100

    UPDATE - updated processing of versions added semver

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as commandLineArgs from "command-line-args";
 import * as fs from "fs";
 import * as childProcess from "child_process";
+import * as semver from "semver";
 
 export interface IHelpObject
 {
@@ -11,6 +12,7 @@ export interface IHelpObject
     majorNumber?: boolean;
     specificVersion?: string;
     targetTag?: string;
+    preReleaseSuffix?: string;
 }
 
 export function processArguments(): IHelpObject
@@ -23,7 +25,8 @@ export function processArguments(): IHelpObject
         { name: "buildNumber", alias: "b", type: Boolean, description: "Indicates that build number of version should be incremented." },
         { name: "majorNumber", alias: "m", type: Boolean, description: "Indicates that major number of version should be incremented." },
         { name: "specificVersion", alias: "v", type: String, description: "Specific version that is going to be set. If this is set overrides any other version parameter.", typeLabel: "<version>" },
-        { name: "targetTag", alias: "t", type: String, description: "Tag that will be assigned to published package. If not specified 'latest' is used for normal version and 'pre' is used for prerelease version.", typeLabel: "<tag>" }
+        { name: "targetTag", alias: "t", type: String, description: "Tag that will be assigned to published package. If not specified 'latest' is used for normal version and 'pre' is used for prerelease version.", typeLabel: "<tag>" },
+        { name: "preReleaseSuffix", alias: "s", type: String, description: "Suffix that will be added to version number. If not specified 'pre' is used. It is not used without 'pre' parameter.", defaultValue: "pre", typeLabel: "<preReleaseSuffix>"}
     ]);
 
     var args: IHelpObject = <IHelpObject>cli.parse();
@@ -46,11 +49,11 @@ export function processArguments(): IHelpObject
                 },
                 {
                     example: '> npp -p',
-                    description: 'Deploys package to npm default repository and sets version as prerelease version. i.e. 1.1.2 => 1.2.0-pre123123123  with tag "pre"'
+                    description: 'Deploys package to npm default repository and sets version as prerelease version. i.e. 1.1.2 => 1.2.0-pre.0  with tag "pre"'
                 },
                 {
                     example: '> npp -p -b',
-                    description: 'Deploys package to npm default repository and sets version as prerelease version. i.e. 1.1.2 => 1.1.3-pre123123123  with tag "pre"'
+                    description: 'Deploys package to npm default repository and sets version as prerelease version. i.e. 1.1.2 => 1.1.3-pre.0  with tag "pre"'
                 },
                 {
                     example: '> npp -b',
@@ -65,8 +68,12 @@ export function processArguments(): IHelpObject
                     description: 'Deploys package to npm default repository and sets specific version. i.e. 1.1.0 => 3.0.0  with tag "latest"'
                 },
                 {
-                    example: '> npp -t "beta"',
-                    description: 'Deploys package to npm default repository and increases minor version number. i.e. 1.1.2 => 1.2.0 with tag "beta"'
+                    example: '> npp -t "stable"',
+                    description: 'Deploys package to npm default repository and increases minor version number. i.e. 1.1.2 => 1.2.0 with tag "stable"'
+                },
+                {
+                    example: '> npp -p -s "beta"',
+                    description: 'Deploys package to npm default repository and increases minor version number and uses "beta" version suffix. i.e. 1.1.2 => 1.2.0-beta.0  with tag "pre"'
                 }
             ]
         }));
@@ -98,7 +105,8 @@ export function publishPackage(args: IHelpObject): void
         }
     }
     
-    childProcess.execSync(command);
+    console.log(command);
+    //childProcess.execSync(command);
 }
 
 export class VersionManager
@@ -106,15 +114,13 @@ export class VersionManager
     //fields
     private _originalVersion: string;
     private _content: string;
-    private _majorVersion: number;
-    private _minorVersion: number;
-    private _buildVersion: number;
+    private _version: string;
     private _preVersion: boolean;
     
     // private properties
     private get VersionRegex(): RegExp
     {
-        return /"version":\s?"(\d+)\.(\d+)\.(\d+)(-\w+)?"/g;
+        return /"version":\s?"(\d+\.\d+\.\d+(-(?:\w|\.)+)?)"/g;
     }
     
     // constructor
@@ -174,26 +180,34 @@ export class VersionManager
     // private methods
     private ComputeVersion(): void
     {
-        if((this._args.pre && this._preVersion) || (!this._args.pre && this._preVersion))
+        if(this._args.pre && this._preVersion)
         {
+            this._version = semver.inc(this._version, "prerelease", this._args.preReleaseSuffix)
+            
             return;
+        }
+        
+        var identifier: string = "";
+        
+        if(this._args.pre)
+        {
+            identifier = "pre";
         }
         
         if(this._args.buildNumber)
         {
-            this._buildVersion++;
+            identifier += "patch";
         }
         else if(this._args.majorNumber)
         {
-            this._majorVersion++;
-            this._minorVersion = 0;
-            this._buildVersion = 0;
+            identifier += "major";
         }
         else
         {
-            this._minorVersion++;
-            this._buildVersion = 0;
+            identifier += "minor";
         }
+        
+        this._version = semver.inc(this._version, identifier, this._args.preReleaseSuffix)
     }
     
     private ReadFile(): void
@@ -220,16 +234,14 @@ export class VersionManager
         {
             var match: RegExpExecArray = this.VersionRegex.exec(this._content);
             
-            this._majorVersion = parseInt(match[1]);
-            this._minorVersion = parseInt(match[2]);
-            this._buildVersion = parseInt(match[3]);
-            this._preVersion = !!match[4];
+            this._originalVersion = this._version = match[1];
+            this._preVersion = !!match[2];
             
-            this._originalVersion = `${match[1]}.${match[2]}.${match[3]}`;
-            
-            if(this._preVersion)
+            if(!semver.valid(this._version))
             {
-                this._originalVersion += match[4];
+                console.error(`Provided version '${this._version}' is not valid according semver specification.`);
+                
+                process.exit(1);
             }
         }
         catch(error)
@@ -246,16 +258,11 @@ export class VersionManager
         
         if(!versionOverride)
         {
-            if(this._args.pre)
-            {
-                var timestamp: number = Math.floor(new Date().getTime() / 1000); 
-                version = `${this._majorVersion}.${this._minorVersion}.${this._buildVersion}-pre${timestamp}`;
-            }
-            else
-            {
-                version = `${this._majorVersion}.${this._minorVersion}.${this._buildVersion}`;
-            }
+            version = this._version;
         }
+        
+        console.log(version);
+        process.exit(1);
         
         var result: string = this._content.replace(this.VersionRegex, `"version": "${version}"`);
 
